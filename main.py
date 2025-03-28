36fd7823497ee3b02bd6f37647edac48b5b8d944
Nicky141012 <nguyen.tran.18294@wellspringsaigon.edu.vn>
2025-03-28 18:59:13 +0700

    finish bouncing

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ pygame.init()
 win_width = 700
 win_height = 500
 img_background = "./assets/background.jpg" 
-
+ball_speedx = -3
+ball_speedy = -3
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
@@ -19,6 +20,12 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+        super().__init__(player_image, player_x, player_y, player_speed, size_x, size_y)
+        self.score = 0
+
+
+
     def update_leftplayer(self):
         keys = key.get_pressed()
         if keys[K_UP] :
@@ -60,6 +67,11 @@ finish =  False
 clock = time.Clock()
 FPS = 60
 
+def draw_ui(x, y, score):
+    my_font = font.Font(None, 36)
+    text = my_font.render("Score:" + str(score) , 1 ,(255, 255, 255))
+    window.blit(text, (x, y))
+
 while game:
         for e in event.get():
             if e.type == QUIT:
@@ -68,6 +80,35 @@ while game:
             window.blit(background,(0,0))
             player_1.update_rightplayer()
             player_2.update_leftplayer()
+
+            draw_ui(0,5,player_1.score)
+            draw_ui(win_width - 100, 5, player_2.score)
+
+            ball.rect.x += ball_speedx
+            ball.rect.y += ball_speedy
+
+            if (ball.rect.y >= win_height - 50):
+                ball_speedy *= -1
+            
+            if (ball.rect.y <= 0 ):
+                ball_speedy *= -1
+            
+            if(sprite.collide_rect(ball, player_1) or sprite.collide_rect(ball, player_2)):
+                ball_speedx *= -1
+            
+            if ball.rect.x > win_width:
+                player_1.score += 1
+                ball.rect.x = 200
+                ball.rect.y = 200
+                ball_speedx *= -1
+                print("Player 1 score 1 point:", player_1.score)
+            if ball.rect.x < 0:
+                player_2.score += 1
+                ball.rect.x = 200
+                ball.rect.y = 200
+                ball_speedx *= -1
+                print("Player 2 score 1 point:", player_2.score)
+
             ball.update()
             
             ball.reset()
